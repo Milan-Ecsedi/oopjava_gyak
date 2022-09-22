05b6add123c2735086e25ab65ca933a633e248bf
Ecsedi <milan200314@gmail.com>
2022-09-22 16:05:21 +0200

    objektum ember osztály
    -3 ember kész

--- src/hu/petrik/helloworld_14S/ember.java
+++ src/hu/petrik/helloworld_14S/ember.java
@@ -13,6 +13,6 @@ public class ember {
 
     @Override
     public String toString() {
-        return String.format("%s");
+        return String.format("%30s %10s %20s", this.nev,this.SzulDatum,this.SzulHely);
     }
 }
